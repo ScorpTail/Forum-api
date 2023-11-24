c7e1c396499a7e0b1baa3bebde20e14ce13487b9
Vladyslav Danchevskyi <scorpio232354@gmail.com>
2023-11-24 22:08:36 +0200

    fix: CORS

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'auth/google/callback', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://ec2-51-20-87-96.eu-north-1.compute.amazonaws.com/'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
